e28d559626cf0b1eb34da50b147d48e90cc43a3d
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2024-09-02 19:54:49 -0300

    renomeação do user id em tasks

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -4,11 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const getTasksByIdUser = (req, res, next) => {
-    const { idUser } = req.body;
+    const { user_id } = req.body;
+
+
 
     const q = "SELECT * FROM tasks where user_id=?";
 
-    db.query(q, [idUser], (err, data) => {
+    db.query(q, [user_id], (err, data) => {
         if (err) return res.status(400).json(err);
         return res.status(200).json(data);
     });
