7369ed45b15bec8c4995806286d85f5a45a6f680
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2024-09-03 16:52:07 -0300

    created method UPDATE for tasks

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -6,8 +6,6 @@ dotenv.config();
 export const getTasksByIdUser = (req, res, next) => {
     const { user_id } = req.body;
 
-
-
     const q = "SELECT * FROM tasks where user_id=?";
 
     db.query(q, [user_id], (err, data) => {
@@ -15,3 +13,34 @@ export const getTasksByIdUser = (req, res, next) => {
         return res.status(200).json(data);
     });
 }
+
+export const createTasks = (req, res, next) => {
+    const { title, description, user_id } = req.body;
+
+    const q = "INSERT INTO tasks (title, description, user_id) VALUES (?, ?, ?);";
+
+    db.query(q, [title, description, user_id], (err, data) => {
+        if (err) {
+            return res.status(400).json(err);
+        }
+
+        return res.status(201).json({ message: "Task created successfully", task: { title, description, user_id } });
+    });
+};
+
+export const updateTasks = (req, res, next) => {
+    const { id } = req.params; // ID da tarefa vem na URL
+    const { title, description, user_id } = req.body; // Dados e ID do usuário no corpo
+
+    const q = "UPDATE tasks SET title = ?, description = ? WHERE id = ? AND user_id = ?;";
+
+    db.query(q, [title, description, id, user_id], (err, result) => {
+        if (err) {
+            return res.status(400).json({ message: "Error updating task", error: err });
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Task not found or user not authorized to update this task" });
+        }
+        return res.status(200).json({ message: "Task updated successfully" });
+    });
+};
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv';
 import express from "express";
-import { getTasksByIdUser } from "../controllers/taskController.js";
+import { createTasks, getTasksByIdUser, updateTasks } from "../controllers/taskController.js";
 
 dotenv.config();
 
 const router = express.Router()
 
 router.get("/getTasksByIdUser", getTasksByIdUser)
+router.post("/createTasks", createTasks)
+router.patch("/updateTasks/:id", updateTasks);
+
 
 export default router;
